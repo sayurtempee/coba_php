e1636ac4e66b1003deac6d428fdd78800ebb8edb
kazuo <kazuo@kazuo-mii>
2024-12-26 17:11:50 +0700

    update login.php

--- login.php
+++ login.php
@@ -4,13 +4,6 @@ error_reporting(E_ALL);
 
 include "connection.php";
 
-session_start();
-// Cek apakah user sudah login
-if (!isset($_SESSION['user_logged_in'])) {
-   header("Location: login.php");
-   exit();
-}
-
 // Cek koneksi database
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
