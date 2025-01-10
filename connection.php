9848f90efd3a2e390b1b660f018b096ddd6c0d07
kazuo <kazuo@kazuo-mii>
2025-01-11 02:57:46 +0700

    edit-all

--- connection.php
+++ connection.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
 // Cek error keseluruhan
 error_reporting(E_ALL);
 
 // Database connection
 $user = "root";
-$pw = "";
+$pw = "mii123"; // jika anda menggunakan password di db nya
 $db = "coba";
 $host = "localhost";
 $conn = mysqli_connect($host, $user, $pw, $db);
-// untuk tes connection
-if ($conn){
-echo 'connect';
-} else {
-echo 'no connect';
-}
-?>
+
+// // untuk tes connection
+// if ($conn) {
+//    echo 'connect';
+// } else {
+//    echo 'no connect';
+// }
